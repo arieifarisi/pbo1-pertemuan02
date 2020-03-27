9fd0b15b128129159129066892bc6841b059bd8b
ariefarisi <mif160399@gmail.com>
2020-03-27 23:49:33 +0800

    Memanggil methid dari Class di Package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ==========================");
+  System.out.println("NPM  : 18631054");
+  System.out.println("Nama : Muhammad Irhash Farisi");
+  System.out.println("============================================");
+ }
+}
